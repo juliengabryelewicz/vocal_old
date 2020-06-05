ad7b9ed973b854cdf5366e416c2d81f2ea412c68
Julien <jgabryelewicz@adexos.fr>
2020-06-05 18:12:55 +0200

    Add tafic plugin

---
+++ plugins/trafic/__init__.py
@@ -0,0 +1,1 @@
+from .trafic import TraficPlugin
---
+++ plugins/trafic/trafic.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import re
+import requests
+import sys
+from vocal import plugin
+import yaml
+
+class TraficPlugin(plugin.PluginObject):
+
+
+    url_bouchons="http://tipi.bison-fute.gouv.fr/publication/cnir/RecapBouchonsFranceEntiere.html"
+
+    def get_response(intent, slots):
+        default = "Je n'ai malheureusement pas bien compris"
+        return getattr(TraficPlugin(), 'case_' + intent, lambda: default)(slots)
+
+    def has_intent(intent):
+        intent_list=["listbouchons"]
+        return any(elem == intent for elem in intent_list)
+
+    def cleanhtml(self, raw_html):
+        cleanr = re.compile('<.*?>')
+        cleantext = re.sub(cleanr, '', raw_html)
+        return cleantext
+
+    def case_listbouchons(self, slots):
+        with open("plugins/trafic/config.yaml") as file:
+            config_list = yaml.load(file, Loader=yaml.FullLoader)
+
+        response=""
+        html_content = requests.get(self.url_bouchons).text
+        soup = BeautifulSoup(html_content, "lxml")
+        departement_table_data = soup.find_all("span", attrs={"class": "rupture"})
+        departement_found=""
+        for departement in departement_table_data:
+            departement_check = departement.find("a", attrs={"name": config_list["departements"].split(",")})
+            if departement_check is not None:
+                departement_found+= str(departement.find_next_sibling("div"))
+        if departement_found == "":
+            return "aucun bouchon à signaler"
+        else:
+            return self.cleanhtml(departement_found)
